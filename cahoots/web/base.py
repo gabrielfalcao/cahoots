eb34f148e048f2cb43580d5d8b986c9a16b07c09
Gabriel Falcão <gabriel@nacaolivre.org>
2020-05-28 00:11:34 +0200

    backend url

--- cahoots/web/base.py
+++ cahoots/web/base.py
@@ -4,12 +4,18 @@ from flask import render_template
 
 from cahoots.logs import set_log_level_by_name
 from cahoots.web.core import application
-from cahoots import config
+# from cahoots import config
 
 
 logger = set_log_level_by_name("DEBUG", __name__)
 
 
+@application.route("/backend", methods=["GET"])
+def backend():
+    return render_template("token-debug.html")
+
+
 @application.route("/", methods=["GET"])
 def index():
     return render_template("token-debug.html")
+    # return render_template("index.html")
